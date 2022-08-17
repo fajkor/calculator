d13d5b63e7991b078e9204af5caaf0c10baa2bee
Faikor Kastrioti <fajkorkastrioti@gmail.com>
2022-08-17 20:59:27 +0200

    Modify script.js
    I added a new class, some new variables and a new eventListener. Modified
    two functions:
    
    * New Class
    Added the class "firstTimeClicked". This class gets added in the DOM only
    if one of the operators (+, -, *, /) is clicked before any of the numbers
    being clicked.
    
    * New Variables
    Added variables "displayValueOne", "displayValueTwo" and "operator".
    These variables are used to store the values and the operator that the
    user enters. After storing them, these values are entered as arguments
    inside the operate() function, in order to calculate the result.
    
    * New eventListener
    Added an eventListener on the Enter button. When Enter gets clicked, a
    calculation is performed and the result is shown on display.
    
    * Modified 2 Functions
    Modified function populateDisplay() and function showOperator().

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const p = document.querySelector(`p`);
 const numberArray = Array.from(document.querySelectorAll(`.number`));
 const operatorArray = Array.from(document.querySelectorAll(`.operator`));
+const enter = document.querySelector(`.enter`);
+let displayValueOne, displayValueTwo, operator;
 
 /* When a number gets clicked show it on the display and update displayValue */
 numberArray.map(number => {
@@ -13,54 +15,58 @@ numberArray.map(number => {
 /* When an operator gets clicked show it on the display and update displayValue */
 operatorArray.map(operator => {
   operator.addEventListener(`click`, () => {
-    operator.classList.add(`operatorClicked`); /* I add the class so I know which operator has been clicked */
-    showOperator();
+    if(p.textContent === `######`){
+      operator.classList.add(`firstTimeClicked`);
+      showOperator();
+    } else {
+      displayValueOne = +p.textContent;
+      operator.classList.add(`operatorClicked`); /* I add the class so I know which operator has been clicked */
+      showOperator();
+    }
   });
 });
 
+enter.addEventListener(`click`, () => {
+  displayValueTwo = +p.textContent;
+  p.textContent = operate(operator, displayValueOne, displayValueTwo);
+})
+
 /* A function to populate the display of the calculator */
 function populateDisplay() {
-    /* An if statement to check if a button has been clicked */
-    /* Every time a button gets clicked, a class='numberClicked' gets added to it.
+    /* An if statement to check if a number button has been clicked */
+    /* Every time a number button gets clicked, a class='numberClicked' gets added to it.
     All we need to do is check if that class exists */
     if(document.querySelector(`.numberClicked`)) { /* if class='numberClicked' exists */
       let numberClicked = document.querySelector(`.numberClicked`);
       if(p.textContent === `######`) {
         p.textContent = numberClicked.textContent;
-        let displayValueOne = +document.querySelector(`p`).textContent;
-        console.log(displayValueOne);
-      } else if(p.textContent === `-` || p.textContent === `+`) { /* I need here to check if a plus or minus operator (-, +) is shown on the display. */
+      } else if(document.querySelector(`.firstTimeClicked`)) { /* I need here to check if a plus or minus operator (-, +) is shown on the display. */
           p.textContent += numberClicked.textContent;
       } else if(p.textContent.includes(`+`) || p.textContent.includes(`-`) || p.textContent.includes(`/`) || p.textContent.includes(`*`)) {
         /* Here the display is updated with the second number. */
         p.textContent = numberClicked.textContent;
-        let displayValueTwo = +document.querySelector(`p`).textContent;
-        console.log(displayValueTwo);
       } else { /* This "else" clause updates the display with the first value we give to the calculator, if the number has more than 1 digits. */
-    p.textContent += numberClicked.textContent;
-    let displayValueOne = +document.querySelector(`p`).textContent;
-    console.log(displayValueOne);
-  }
+        p.textContent += numberClicked.textContent;
+      }
       numberClicked.classList.remove(`numberClicked`); /* !Important! numberClicked class, needs to be removed. It served its purpose. Now this same class needs to be added to another number button (or to the same). */
     } 
+    return p.textContent;
   }
 
   function showOperator() {
     if (document.querySelector(`.operatorClicked`)) {
-        let operatorClicked = document.querySelector(`.operatorClicked`);
-        if(p.textContent === `######`){ /* I need this "if" clause in case the user clicks an operator before clicking a number */
-          p.textContent = operatorClicked.textContent;
-        } else {
-          // let displayValueOne = +document.querySelector(`p`).textContent;
-          document.querySelector(`p`).textContent = document.querySelector(`p`).textContent + ` ` + document.querySelector(`.operatorClicked`).textContent;
-        }
-        operatorClicked.classList.remove(`.operatorClicked`);
-        operatorArray.map(operator => {
-          operator.disabled = true;
-        });
+      let operatorClicked = document.querySelector(`.operatorClicked`);
+      p.textContent = p.textContent + ` ` + operatorClicked.textContent;
+      operator = operatorClicked.textContent;
+      operatorClicked.classList.remove(`operatorClicked`);
+      if(document.querySelector(`.firstTimeClicked`)) {
+        document.querySelector(`.firstTimeClicked`).classList.remove(`firstTimeClicked`);
+      }
+    } else {
+      let firstTimeClicked = document.querySelector(`.firstTimeClicked`);
+      p.textContent = firstTimeClicked.textContent;
     }
   }
-
 
 function removeElement(parentElement, childElement) {
   parentElement.removeChild(childElement);
